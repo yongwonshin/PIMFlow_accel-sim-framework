bb8566711858aadc4527c640cf619667896a1f02
Juseong <wntjd9805@postech.ac.kr>
2022-06-30 09:22:14 +0000

    cleanup

--- util/tracer_nvbit/run2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#export DYNAMIC_KERNEL_LIMIT_START=53
-#export DYNAMIC_KERNEL_LIMIT_END=82
-
-#export CUDA_VISIBLE_DEVICES=0
-LD_PRELOAD=./tracer_tool/tracer_tool.so python3 /root/pact2022/$1/$2.py "${@:3}"
-
-
-
